608077b7046c11eb9c47dc5c73d70bb29c3bb876
Gael Jean-albert <jng5087a@ensfsi.onmicrosoft.com>
2024-09-18 14:59:42 +0200

    Fin classe Village

---
+++ src/personnages/Druide.java
@@ -0,0 +1,30 @@
+package personnages;
+
+public class Druide {
+
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
+				+ effetPotionMax + ".");
+
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	private void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le druide " + nom + " : "; 
+	}
+
+}
